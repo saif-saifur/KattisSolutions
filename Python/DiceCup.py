1ffcc1dd4cef5c1e7c71d1c1e99a562ecb8d2104
Saifur Rahman <msaifur92@gmail.com>
2023-01-30 17:09:21 +0100

    Update DiceCup.py

--- Python/DiceCup.py
+++ Python/DiceCup.py
@@ -1,1 +1,13 @@
+def main():
+    N, M = map(int, input().split())
+    outcomes = [0] * (N + M + 1)
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            outcomes[i + j] += 1
+    max_outcome = max(outcomes)
+    for i, outcome in enumerate(outcomes):
+        if outcome == max_outcome:
+            print(i)
 
+if __name__ == "__main__":
+    main()
